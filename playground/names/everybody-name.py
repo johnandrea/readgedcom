6c3cbb6e02905e8d6b42717e5f0386b02b7ce81c
johnandrea <46268748+johnandrea@users.noreply.github.com>
2022-05-14 15:28:54 -0300

    Update everybody-name.py

--- playground/names/everybody-name.py
+++ playground/names/everybody-name.py
@@ -1,4 +1,4 @@
-import os
+import sys
 import readgedcom
 
 def show( indi, info, out_file ):
@@ -6,15 +6,8 @@ def show( indi, info, out_file ):
     if '&' in html:
        print( indi, info['display'], '/', html, '/', info['unicode'], file=out_file ) 
 
-there = '..' + os.path.sep + '..' + os.path.sep + 'families' + os.path.sep
+data = readgedcom.read_file( sys.argv[1] )
 
 with open( 'names.out', 'w' ) as outf:
-     for family in ['andrea','rogers','smith']:
-         ged = there + family + os.path.sep + 'data' + os.path.sep + family + '.ged'
-
-         if os.path.isfile( ged ):
-            data = readgedcom.read_file( ged )
-            for indi in data[readgedcom.PARSED_INDI]:
-                show( indi, data[readgedcom.PARSED_INDI][indi]['name'][0], outf )
-         else:
-            print( 'coundnt', ged, file=outf )
+     for indi in data[readgedcom.PARSED_INDI]:
+         show( indi, data[readgedcom.PARSED_INDI][indi]['name'][0], outf )
